e5dce3e570d5e6f640f01ad69dd555af37969264
Edivaldo <edivaldo327@gmail.com>
2017-10-17 10:22:18 -0200

    Inicio do projeto

--- app/src/main/java/com/example/aluno/appbd/MainActivity.java
+++ app/src/main/java/com/example/aluno/appbd/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.aluno.appbd;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button bCadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bCadastro=(Button)findViewById(R.id.bCadastro);
+
+        bCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CadastroActivity.class));
+            }
+        });
     }
 }
